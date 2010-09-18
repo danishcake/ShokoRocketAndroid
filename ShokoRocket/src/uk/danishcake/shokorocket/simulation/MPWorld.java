26488de75b7743cde1d573718d952ea5e9c1c5b6
unknown <Edward@.(none)>
2010-09-18 10:44:54 +0100

    Added MPWorld class

---
+++ ShokoRocket/src/uk/danishcake/shokorocket/simulation/MPWorld.java
@@ -0,0 +1,13 @@
+package uk.danishcake.shokorocket.simulation;
+
+import org.w3c.dom.Element;
+
+public class MPWorld extends WorldBase {
+
+	@Override
+	protected void loadSpecific(Element root) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ShokoRocketTest/src/uk/danishcake/shokorocket/test/MPWorldTests.java
@@ -0,0 +1,12 @@
+package uk.danishcake.shokorocket.test;
+
+import uk.danishcake.shokorocket.simulation.MPWorld;
+import android.test.AndroidTestCase;
+
+public class MPWorldTests extends AndroidTestCase {
+	public void testMPWorldBasics()
+	{
+		//Really just checks MPWorld class exists
+		MPWorld world = new MPWorld();
+	}
+}
